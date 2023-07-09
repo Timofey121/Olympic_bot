a7d9988f24db97225e07a06c75a08f326fb26df5
“Timofey121” <ale3jurtaev@gmail.com>
2023-07-09 13:25:13 +0400

    Timofey: working version 1.15

--- main_olympiads.py
+++ main_olympiads.py
@@ -1,7 +1,6 @@
 import asyncio
 
 from data.config import ADMINS
-from handlers.users.notification import check
 from loader import dp
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -12,19 +11,9 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
-async def test(dispatcher):
-    while True:
-        try:
-            await check(dispatcher)
-        except Exception as ex:
-            pass
-        await asyncio.sleep(43200)
-
-
 async def main():
     for item in ADMINS:
         await dp.bot.send_message(item, 'Бот запущен!')
-    asyncio.create_task(test(dp))
     await dp.start_polling(on_startup(dp))
 
 
